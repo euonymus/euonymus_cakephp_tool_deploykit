e4e12a349c4f027b37f1d1f046442337bd0458c6
euonymus <euonymus0220@gmail.com>
2014-10-29 12:35:24 +0900

    first commit

---
+++ chef/site-cookbooks/test_site/recipes/mysql.rb
@@ -0,0 +1,47 @@
+include_recipe "database::mysql"
+
+
+# This is used repeatedly, so we'll store it in a variable
+mysql_connection_info = {
+  host:     'localhost',
+  username: 'root',
+  password: node['mysql']['server_root_password']
+}
+
+# Ensure a database exists with the name of our app
+mysql_database node[:test_site][:db_name] do
+  connection mysql_connection_info
+  action     :create
+end
+mysql_database node[:test_site][:testdb_name] do
+  connection mysql_connection_info
+  action     :create
+end
+
+# Ensure a database user exists with the name of our app
+mysql_database_user node[:test_site][:db_user] do
+  connection mysql_connection_info
+  password   node[:test_site][:db_password]
+  action     :create
+end
+mysql_database_user node[:test_site][:testdb_user] do
+  connection mysql_connection_info
+  password   node[:test_site][:db_password]
+  action     :create
+end
+
+# Let this database user access this database
+mysql_database_user node[:test_site][:db_user] do
+  mysql_connection_info
+  password      node[:test_site][:db_password]
+  database_name node[:test_site][:db_name]
+  host          'localhost'
+  action        :grant
+end
+mysql_database_user node[:test_site][:testdb_user] do
+  mysql_connection_info
+  password      node[:test_site][:db_password]
+  database_name node[:test_site][:testdb_name]
+  host          'localhost'
+  action        :grant
+end
